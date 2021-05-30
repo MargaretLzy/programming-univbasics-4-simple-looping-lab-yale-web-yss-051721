ecd37bac1300bde4babc8c58e6ce1bd839afc094
dynamic-daemon-637b <zl3087@columbia.edu>
2021-05-30 20:28:43 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -7,4 +7,10 @@ def loop_message_five_times(string)
   end
 end
 def loop_message_n_times(string, integer)
-  
+  c=0
+    while c<integer do
+    puts string
+    c +=1
+  end
+def output_array(array)
+  c
